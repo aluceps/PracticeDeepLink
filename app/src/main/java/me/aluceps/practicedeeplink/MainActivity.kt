f958c02c4b2c3f397ca38209564aa8598834e9e1
Atsushi Takano <aluceps@gmail.com>
2019-07-04 02:38:04 +0900

    Apply databinding

--- app/src/main/java/me/aluceps/practicedeeplink/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicedeeplink/MainActivity.kt
@@ -1,12 +1,18 @@
 package me.aluceps.practicedeeplink
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.practicedeeplink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
